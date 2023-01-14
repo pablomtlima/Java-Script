3689b2eead8b902bd28e5c3a453d4eb277eeb1d3
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-01-14 16:30:48 -0300

    Aula 4

---
+++ Aula 4/app.js
@@ -0,0 +1,45 @@
+// // //boolean e comparações
+// // // console.log(true, false, 'true', 'fault')
+
+// // //Métodos podem retornar booleans
+// // const email = 'pablomtlima1@gmail.com'
+// // const includes = email.includes('wayne')
+// // const names = ['dio', 'pablo', 'robert']
+// // const arrayIncludes = names.includes('pablo')
+
+// // // console.log(arrayIncludes)
+
+// // const age = 31 
+// // const name = 'a'
+
+// // // console.log(age == 31)
+// // // console.log(age == 35)
+// // // console.log(age != 31)
+// // // console.log(age > 30)
+// // // console.log(age < 32)
+// // // console.log(age <= 31)
+// // // console.log(age >= 31)
+
+// // console.log(name == 'roger')
+// // console.log(name == 'Roger')
+// // console.log(name > 'belinha')
+// // console.log(name < 'A')
+// // console.log(name > 'Belinha')
+
+// const age = 31
+
+// // console.log(age == 31)
+// // console.log(age == '31')
+// // console.log(age != 31)
+// // console.log(age != '31')
+
+// // console.log(age === 31)
+// // console.log(age === '31')
+
+// console.log(age !== 31)
+// console.log(age !== '31')
+
+
+
+//Conversão de tipos
+let score
