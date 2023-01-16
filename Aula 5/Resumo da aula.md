9f475666a4c110682aaf160c3820bce2acbd1067
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-01-15 22:21:55 -0300

    22:21

---
+++ Aula 5/Resumo da aula.md
@@ -0,0 +1,7 @@
+cria um loop dentro do for pode-se implementar um contador
+
+```js
+for (let i = 0 ; i < 5; i++){
+    
+}
+```
---
+++ Aula 5/app.js
@@ -0,0 +1,35 @@
+// for (let i = 0 ; i < 5; i++){
+//     console.log(`Dentro do loop ${i}`)
+// }
+// console.log(`Fora do loop `)
+
+// const names = ['Linus', 'Ada', 'Bill', 'Gates', 'Pablo', 'Matos', 'Fabina', 'Pedro']
+
+// for (let i = 0; i < names.length ; i++){
+//     const HTMLTemplate = `<p>${names[i]}</p>`
+//     console.log(HTMLTemplate)
+// }
+
+// const frase = ('A carolina não sabe por quanto tempo vai ficar na cidade.')
+
+// for (i = 0; i < frase.length; i++){
+//     const iterandoAFrase = frase[i]
+//     console.log(iterandoAFrase)
+// }
+
+//while loop
+
+// i=0
+
+// while (i < 5 ){
+//     console.log(`Dentro do loop: ${i}`)
+//     i++
+// }
+
+const names = ['Linus', 'Ada', 'Bill', 'Gates', 'Pablo', 'Matos', 'Fabina', 'Pedro']
+let i = 0 
+
+while (i < names.length){
+    console.log(names[i])
+    i++
+}
