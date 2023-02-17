334467b621307be595b4693ae748783a881656c3
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-02-16 23:15:27 -0300

    refatorado

--- TODO/app.js
+++ TODO/app.js
@@ -1,94 +1,57 @@
-const formAddTodo = document.querySelector('.form-add-todo')
-const todosContainer = document.querySelector('.todos-container')
-const formSearch = document.querySelector('.form-search')
+const formAddTodo = document.querySelector(".form-add-todo");
+const todosContainer = document.querySelector(".todos-container");
+const formSearch = document.querySelector(".form-search");
 
+const addTodo = addInput => {
+    if (addInput.length) {
+        todosContainer.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${addInput}">
+        <span>${addInput}</span>
+        <i class="far fa-trash-alt" data-trash="${addInput}"></i>
+        </li>`
 
-formAddTodo.addEventListener('submit', event =>{
-    event.preventDefault()
-    const addInput = event.target.add.value.trim()
-
-    if(addInput){
-    todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${addInput}</span>
-    <i class="far fa-trash-alt delete"></i>
-    </li>`}
-
-    event.target.add.value = ''
-
+        event.target.reset()
+    }   
+}
+formAddTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const addInput = event.target.add.value.trim();
+    addTodo(addInput)
 })
 
-todosContainer.addEventListener('click', event=>{
-    const elementoClicado = event.target
-    if (Array.from(elementoClicado.classList).includes('delete')){
-        elementoClicado.parentElement.remove()
-    }    
-})  
+const removeTodo = elementoClicado => {
+    const trashDataValue = elementoClicado.dataset.trash
+    const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
+  
+    if (trashDataValue) {
+      todo.remove()
+    }
+}
 
-formSearch.addEventListener('input', event => {
-    event.preventDefault()
-    
-    const inputSearch = event.target.value.trim()
+todosContainer.addEventListener("click", event => {
+  const elementoClicado = event.target
+  removeTodo(elementoClicado)
+});
 
-    Array.from(todosContainer.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(inputSearch))
-    .forEach(todo => {
-        todo.classList.add('hidden')
-        todo.classList.remove('d-flex')
-    })
+formSearch.addEventListener("input", (event) => {
+  event.preventDefault();
 
-    Array.from(todosContainer.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(inputSearch))
-    .forEach(todo => {
-        todo.classList.remove('hidden')
-        todo.classList.add('d-flex')
-        console.log('FINALIZADO')
-    })  
-})
+  const inputSearch = event.target.value.trim();
+
+  Array.from(todosContainer.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(inputSearch))
+    .forEach((todo) => {
+      todo.classList.add("hidden");
+      todo.classList.remove("d-flex");
+    });
+
+  Array.from(todosContainer.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(inputSearch))
+    .forEach((todo) => {
+      todo.classList.remove("hidden");
+      todo.classList.add("d-flex");
+      console.log("FINALIZADO");
+    });
+});
 
 // foco Pablo, foco, um dia chego lá
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// formSearch.addEventListener('input', event=>{
-//     event.preventDefault()
-//     const inputSearch = event.target.value.trim()
-    
-//     console.log(inputSearch)  
-
-//     Array.from(todosContainer.children)
-//     .filter(todo => !todo.textContent.includes(inputSearch))
-//     .forEach( todo => {
-//         todo.classList.remove('d-flex')
-//         todo.classList.add('hidden')
-//         console.log(inputSearch.textContent)
-//     })
-
-
-// })
-
