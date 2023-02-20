22b3c556d03de63570441e6dce5167bcfe115d98
Pablo Matos Lima <pablomtlima1@gmail.com>
2023-02-19 23:04:59 -0300

    Contruindo relogio digital

---
+++ Construindo um relogio digital/script.js
@@ -0,0 +1,15 @@
+const clockContainer = document.querySelector('.clock-container')
+
+
+
+const updateClock = () => {
+    const present = new Date()
+    const hours = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
+    clockContainer.innerHTML = `
+    <spam> ${String(hours).length === 1 ? '0' + hours : hours}</spam> :
+    <spam> ${String(minutes).length === 1 ?'0' + minutes : minutes}</spam> :
+    <spam> ${String(seconds).length === 1 ? '0' + seconds : seconds}</spam>`
+}
+setInterval(updateClock, 1000)
