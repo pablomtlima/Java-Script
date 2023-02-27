029f80e95375bc1c427fec37c593721e52f2d932
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-02-27 19:46:09 -0300

    atualização

--- Fazendo requests HTTP/app.js
+++ Fazendo requests HTTP/app.js
@@ -1,29 +1,48 @@
-const getTodos = (url, callback) => { 
+const getTodos = (url, callback) => new Promise ( (resolve, reject) =>{    
     const request = new XMLHttpRequest()
-
+    
     request.addEventListener('readystatechange', ()=>{
-
+    
         if(request.readyState === 4 && request.status === 200){
             const data = JSON.parse(request.responseText)
-            callback(null, data)
-            return
+            resolve(data)
         }
         if(request.readyState === 4 && request.status !== 200){
-            callback('Deu ruim nos dados', null)
-            return
+            reject('Deu ruim nos dados')
         }
     })
-
     request.open('GET', url)
     request.send()
-}
+}) 
 
-getTodos('./json/todos.json',(error, data) => {
-    console.log(data)
-    getTodos('./json/todos-02.json', (error,data) => {
-        console.log(data)
-        getTodos('./json/todos-03.json',(error,data)=>{
-            console.log(data)
-        })
-    })
-})
+console.log(1)
+
+getTodos('https://pokeapi.co/api/v2/pokemon/ditto')
+.then(pokemon => console.log(pokemon))
+.catch(error => console.log(error))
+
+console.log(1)
+
+getTodos('https://pokeapi.co/api/v2/pokemon/pikachu')
+.then(pokemon => console.log(pokemon))
+.catch(error => console.log(error))
+
+console.log(1)
+
+getTodos('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+.then(pokemon => console.log(pokemon))
+.catch(error => console.log(error))
+
+
+console.log(1)
+
+// getTodos('./json/todos.json',(error, data) => {
+//     console.log(data)
+//     getTodos('./json/todos-02.json', (error,data) => {
+//         console.log(data)
+//         getTodos('./json/todos-03.json',(error,data)=>{
+//             console.log(data)
+//         })
+//     })
+// })
+
