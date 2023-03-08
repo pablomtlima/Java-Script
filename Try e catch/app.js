4fa1db87b6dc84ae8ad1716b35c76451d34f2413
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-03-07 22:48:21 -0300

    Tratando erros

--- Try e catch/app.js
+++ Try e catch/app.js
@@ -1,11 +1,32 @@
-try {
-    console.log(oi)
-} catch (error) {
-    
-    if(error.name == 'ReferenceError' && error.message == 'oi is not defined' ){
-        const oi = 'Oi criada'
-        console.log(oi)
-    }
+const getUsers = async () => {
+    try{
+        const response = await fetch('../json/todos.json')
 
-    console.log(error.message, error.name)
+        if(!response.ok){
+            throw new Error('Não foi possivel obter os dados')
+        }
+        
+    }catch (error){
+        console.log(error.message)
+    }
 }
+
+
+const logUsers = async () => {
+    const users = await getUsers()
+    console.log(users)
+}
+
+logUsers()
+
+// try {
+//     console.log(oi)
+// } catch (error) {
+
+//     if(error.name == 'ReferenceError' && error.message == 'oi is not defined' ){
+//         const oi = 'Oi criada'
+//         console.log(oi)
+//     }
+
+//     console.log(error.message, error.name)
+// }
