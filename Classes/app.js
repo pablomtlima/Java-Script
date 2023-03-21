ee62fdd2a447c9ed5e57d496884f3539766863ed
Pablo Matos Lima <pablomtlima1@gmail.com>
2023-03-21 13:30:32 -0300

    att place holder

--- Classes/app.js
+++ Classes/app.js
@@ -1,68 +1,68 @@
-class User {
-    constructor (name, email, age) {
-        this.name =  name
-        this.email = email
-        this.age = age
-        this.points = 0
-    }
-    // criar os métodos fora do constructor
-    login () {
-        console.log(`${this.name} logou na aplicação`)
-        return this
-    }
+// class User {
+//     constructor (name, email, age) {
+//         this.name =  name
+//         this.email = email
+//         this.age = age
+//         this.points = 0
+//     }
+//     // criar os métodos fora do constructor
+//     login () {
+//         console.log(`${this.name} logou na aplicação`)
+//         return this
+//     }
 
-    logout () {
-        return `${this.name} deslogou da aplicação`
-    }
+//     logout () {
+//         return `${this.name} deslogou da aplicação`
+//     }
 
-    addPoints () {
-        this.points++
-        return `${this.name} agora tem ${this.points > 1 ? 'pontos' : 'ponto'}`
-    }
-}
+//     addPoints () {
+//         this.points++
+//         return `${this.name} agora tem ${this.points > 1 ? 'pontos' : 'ponto'}`
+//     }
+// }
 
-const user = new User('Pablo', 'pablomtlima1@gmail.com',25)
-const user2 = new User('Sofia', 'sofia@gmail.com',2)
+// const user = new User('Pablo', 'pablomtlima1@gmail.com',25)
+// const user2 = new User('Sofia', 'sofia@gmail.com',2)
 
-user.login().addPoints()
+// user.login().addPoints()
 
-console.log(user, user2)
-
-
+// console.log(user, user2)
 
 
-class Mammal {
-    constructor (species, name, age) {
-        this.species = species
-        this.name = name 
-        this.age = age
-        this.mammaryGland = true
-    }
 
-    incrementAge () {
-        this.age++
-    }
-}
 
-//  extends faz com que Lion herde todas as propriedades e métodos de Mammal
-class Lion extends Mammal{
-    constructor (species, name, age, manEater) {
-        super  (species, name, age)
-        this.manEater = manEater
-    }
+// class Mammal {
+//     constructor (species, name, age) {
+//         this.species = species
+//         this.name = name 
+//         this.age = age
+//         this.mammaryGland = true
+//     }
 
-    eatZebras (animals) {
-        return animals.filter(animals => animals.species !== 'zebra')
-    }
-}
-const zeca = new Mammal('zebra', 'Zeca', 5)
-const pompeu = new Mammal('gnu', 'Pompeu', 4)
-const angus = new Mammal('cavalo', 'Angus', 3)
-const mufaza = new Lion('mufaza', 'Leão', 9, false)
+//     incrementAge () {
+//         this.age++
+//     }
+// }
 
-const animals = [zeca, pompeu, angus]
+// //  extends faz com que Lion herde todas as propriedades e métodos de Mammal
+// class Lion extends Mammal{
+//     constructor (species, name, age, manEater) {
+//         super  (species, name, age)
+//         this.manEater = manEater
+//     }
 
-console.log(zeca, pompeu, angus, mufaza)
-console.log(mufaza.eatZebras(animals))
+//     eatZebras (animals) {
+//         return animals.filter(animals => animals.species !== 'zebra')
+//     }
+// }
+// const zeca = new Mammal('zebra', 'Zeca', 5)
+// const pompeu = new Mammal('gnu', 'Pompeu', 4)
+// const angus = new Mammal('cavalo', 'Angus', 3)
+// const mufaza = new Lion('mufaza', 'Leão', 9, false)
+
+// const animals = [zeca, pompeu, angus]
+
+// console.log(zeca, pompeu, angus, mufaza)
+// console.log(mufaza.eatZebras(animals))
 
 
---
+++ Funções construtoras/app.js
@@ -0,0 +1,19 @@
+// class Student {
+//     constructor (name, email) {
+//         this.name = name
+//         this.email = email
+//     }
+// }
+
+
+// this dentro de uma arrow func não representa o escopo dentro da função
+// mas o escopo onde a arrow func foi declarada.
+
+function Student (name, email) {
+    this.name = name
+    this.email = email
+}
+
+
+const gabrielFialho = new Student('Gabriel Fialho', 'gabrielf@roger.com.br')
+const brenoLemos = new Student('Breno Lemos', 'brenol@rgoe.com.br')
