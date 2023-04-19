0177c3a876db4bab0aceedfba708bfd7f8bd4b45
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-04-18 23:41:20 -0300

    ~refatoração da ultima aplicação

--- Conectando ao firebase/app.js
+++ Conectando ao firebase/app.js
@@ -1,7 +1,7 @@
-  import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
-  import { getFirestore, collection, addDoc, serverTimestamp, doc, deleteDoc, onSnapshot} from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+import { getFirestore, collection, addDoc, serverTimestamp, doc, deleteDoc, onSnapshot } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
 
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: 'AIzaSyCho-kJz3S7VziTD6gtT11ulUpGiAU8Mic',
     authDomain: 'testing-firebase-793e3.firebaseapp.com',
     projectId: 'testing-firebase-793e3',
@@ -9,60 +9,69 @@
     messagingSenderId: '959471524008',
     appId: '1:959471524008:web:ab9464de5e528393000a9d',
     measurementId: 'G-6Z8PQMTX6M'
-  }
+}
 
-  const app = initializeApp(firebaseConfig)
-  const db = getFirestore(app)
-  const collectionGames = collection(db, 'games')
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const collectionGames = collection(db, 'game')
 
-  const formAddGame = document.querySelector('[data-js="add-game-form"]')
-  const gamesList = document.querySelector('[data-js="games-list"]')
-  const unsubscribeButton = document.querySelector('[data-js="unsub"]')
+const formAddGame = document.querySelector('[data-js="add-game-form"]')
+const gamesList = document.querySelector('[data-js="games-list"]')
+const unsubscribeButton = document.querySelector('[data-js="unsub"]')
 
-  const unsubscribe =  onSnapshot(collectionGames, querySnapshot => {
-    console.log('CallBack do onSnapShot executado')
-    if(!querySnapshot.metadata.hasPendingWrites){
-      const games = querySnapshot.docs.reduce((acc, doc, index) => { 
-        const {title, developedBy, createdAt} = doc.data()
-      acc += `<li data-id="${doc.id}" class="my-4">
-        <h5>${title}</h5>
-  
-        <ul>
-          <li>Desenvolvido por ${developedBy} </li>
-          ${createdAt 
-            ? `<li>Adicionado no banco em ${createdAt.toDate()}</li>` : ''}
+const formatDate = createdAt => new Intl
+    .DateTimeFormat('pt-BR', { dateStyle: 'short', timeStyle: 'short' })
+    .format(createdAt.toDate())
 
-        </ul> 
-        <button data-remove="${doc.id}" class="btn btn-danger btn-sm">Remover</button>
-      </li>`
-      return acc
-    }, '')
-    console.log(games)
-    gamesList.innerHTML = games
-    console.log('Manipulação de DOM executada')
+const renderGamesList = querySnapshot => {
+    if (!querySnapshot.metadata.hasPendingWrites) {
+        gamesList.innerHTML = querySnapshot.docs.reduce((acc, doc ) => {
+            const [id, { title, developedBy, createdAt }] = [doc.id, doc.data()]
+
+            return `${acc}<li data-id="${id}" class="my-4">
+                <h5>${title}</h5>
+        
+                <ul>
+                <li>Desenvolvido por ${developedBy} </li>
+                ${createdAt
+                    ? `<li>Adicionado no banco em ${formatDate(createdAt)}</li>` : ''}
+
+                </ul> 
+                <button data-remove="${id}" class="btn btn-danger btn-sm">Remover</button>
+            </li>`
+        }, '')
     }
+}
 
-})
-
-  formAddGame.addEventListener('submit', e =>{
+const addGame = e => {
     e.preventDefault()
 
     addDoc(collectionGames, {
-      title: e.target.title.value,
-      developedBy: e.target.developer.value,
-      createdAt: serverTimestamp()
+        title: e.target.title.value,
+        developedBy: e.target.developer.value,
+        createdAt: serverTimestamp()
     })
-    .then(doc => console.log('Documento criado com o ID', doc.id))
-    .catch(console.log)
-  })
-
-  gamesList.addEventListener('click', e => {
-    const idRemoveButton = e.target.dataset.remove
-    if(idRemoveButton) {
-      deleteDoc(doc(db, 'games', idRemoveButton))
-        .then(() => console.log('Game removido'))
+        .then(doc => {
+            console.log('Documento criado com o ID', doc.id)
+            e.target.reset()
+            e.target.title.focus()
+        })
         .catch(console.log)
-    }
-  })
 
-  unsubscribeButton.addEventListener('click', unsubscribe)
+}
+
+const deletGame = e => {
+    const idRemoveButton = e.target.dataset.remove
+    if (idRemoveButton) {
+        deleteDoc(doc(db, 'games', idRemoveButton))
+            .then(() => console.log('Game removido'))
+            .catch(console.log)
+    }
+}
+
+const handleSnapshotError = e => console.log(e)
+
+const unsubscribe = onSnapshot(collectionGames, renderGamesList, handleSnapshotError)
+formAddGame.addEventListener('submit', addGame)
+gamesList.addEventListener('click', deletGame)
+unsubscribeButton.addEventListener('click', unsubscribe)
